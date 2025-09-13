4df7c5089c876eb5a491ea2470d3626cf0b23db9
reaper <gabrieljaivona@gmail.com>
2025-09-13 11:23:46 -0400

    Caixa de boas vindas feita

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" >
       <body>{children}</body>
     </html>
   );
--- src/components/BoxWelcome.tsx
+++ src/components/BoxWelcome.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 
-
-export default function BoxWelcome () {
-    return(
-        <div className="bg-[#283593] h-50 flex justify-center items-center w-screen m-2 rounded-xs">
-            <h1 className="text-6xl font-medium">Bem-vindo ao OlimpoMatematica!</h1>
-        </div>
-    )
+export default function BoxWelcome() {
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center bg-[#283593] h-50  w-screen m-2 rounded-xs">
+      <h1 className="text-4xl font-medium">Bem-vindo ao OlimpoMatematica!</h1>
+      <p>Sua jornada para o sucesso nas Olimpíadas de Matemática começa aqui.</p>
+      <Link
+        href="/auth"
+        className="inline-flex items-center gap-2 bg-[#FF9800] font-bold py-3 px-6 rounded-lg shadow-md
+hover:bg-[#BF8841] hover:text-white transition-all duration-300
+          transform hover:scale-105">Inscreva-se agora
+      </Link>
+    </div>
+  );
 }
